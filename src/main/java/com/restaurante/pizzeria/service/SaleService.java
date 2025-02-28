56b688200847d25eecd5fbb88c91ec6e26322fe1
zareangel <miguelaz1@outlook.com>
2025-02-27 19:10:40 -0500

    Se arreglo la venta en stock

--- src/main/java/com/restaurante/pizzeria/service/SaleService.java
+++ src/main/java/com/restaurante/pizzeria/service/SaleService.java
@@ -4,6 +4,7 @@ import com.restaurante.pizzeria.entity.Product;
 import com.restaurante.pizzeria.entity.Sale;
 import com.restaurante.pizzeria.repository.ProductRepository;
 import com.restaurante.pizzeria.repository.SaleRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,6 +19,25 @@ public class SaleService {
         this.saleRepository = saleRepository;
         this.productRepository = productRepository;
     }
+    @Transactional
+    public Sale saveSale(Sale sale) {
+
+        Product product = productRepository.findById(sale.getProduct().getId())
+                .orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+        if (!product.isStatus()) {
+            throw new RuntimeException("El producto " + product.getName() + " no está disponible para la venta.");
+        }
+        if (product.getStock() < sale.getQuantity()) {
+            throw new RuntimeException("Stock insuficiente para el producto: " + product.getName());
+        }
+        product.setStock(product.getStock() - sale.getQuantity());
+        if (product.getStock() == 0) {
+            product.setStatus(false);
+        }
+        productRepository.save(product);
+        return saleRepository.save(sale);
+    }
+
     public Optional<Sale> findSaleByNumberSale(String numberSale) {
         return saleRepository.findByNumberSale(numberSale);
     }
@@ -25,33 +45,6 @@ public class SaleService {
     public List<Sale> findAllSales() {
         return saleRepository.findAll();
     }
-
-    public Sale saveSale(Sale sale) {
-        System.out.println("Venta recibida: " + sale);
-
-        if (sale.getProduct() == null || sale.getProduct().getId() == null) {
-            throw new IllegalArgumentException("El producto es obligatorio.");
-        }
-
-        Product product = productRepository.findById(sale.getProduct().getId())
-                .orElseThrow(() -> new RuntimeException("Producto no encontrado"));
-
-
-        if (product.getStock() < sale.getQuantity()) {
-            throw new IllegalArgumentException("Stock insuficiente para la venta.");
-        }
-
-        product.setStock(product.getStock() - sale.getQuantity());
-
-        if (product.getStock() == 0) {
-            product.setStatus(false);
-        }
-
-        productRepository.save(product);
-        return saleRepository.save(sale);
-    }
-
-
 
 
 }
